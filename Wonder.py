08091eb2900640ec07baf41baad1189732b1ce59
ClémentValot <clement.valot.pro@gmail.com>
2021-11-13 00:01:03 +0100

    Start project

---
+++ Wonder.py
@@ -0,0 +1,12 @@
+from Effectable import Effectable
+
+
+class Wonder:
+    def __init__(self):
+        self.levels = []
+
+
+class WonderLevel(Effectable):
+    def __init__(self):
+        
+        self.cost = 0
